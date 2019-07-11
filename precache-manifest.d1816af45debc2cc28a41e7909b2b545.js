1d3b721f60bc20b9fbe7b3c3fd6e71da786ad02b
Andres Rpo <andresr.alz@gmail.com>
2019-07-10 21:32:36 -0500

    Updates

---
+++ precache-manifest.d1816af45debc2cc28a41e7909b2b545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb447d5334d21452a92b08c69587a60",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "6c0c414302368e4801f8",
+    "url": "/tictactoe/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "66ad0df32f1229b269c7",
+    "url": "/tictactoe/static/css/main.5e44ae1b.chunk.css"
+  },
+  {
+    "revision": "6c0c414302368e4801f8",
+    "url": "/tictactoe/static/js/2.74e2ebb9.chunk.js"
+  },
+  {
+    "revision": "66ad0df32f1229b269c7",
+    "url": "/tictactoe/static/js/main.40a0f657.chunk.js"
+  },
+  {
+    "revision": "e9d42febdd2440e2e0cd",
+    "url": "/tictactoe/static/js/runtime~main.1cfd47c4.js"
+  }
+]);
